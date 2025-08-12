5fcf9ea6d3efdbafb3757b68695c51e23098327e
Komsan-Kongsuwan <komsan.kongsuwan@gmail.com>
2025-08-12 15:05:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.set_page_config(page_title="Monthly Report", layout="wide")
-st.title("📊 Welcome to the Monthly Report App")
+st.title("📊 Test monthly revenue by customer")
